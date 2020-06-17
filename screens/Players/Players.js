1cafd8bc62f462a2ca3771134670fd8be91e3014
Phong Nguyen <phongnhh@Phongs-Mac-mini.local>
2020-06-17 10:03:31 +0700

    add Player screen, build demo

--- screens/Players/Players.js
+++ screens/Players/Players.js
@@ -2,15 +2,19 @@ import React, {useEffect} from 'react';
 import {View, StyleSheet, FlatList, Text} from 'react-native';
 import {getPlayers} from '../../utils/callApi';
 import useAsync from '../../utils/useAsync';
+import PlayerCard from './components/PlayerCard';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgb(214, 205, 221)',
     flex: 1,
   },
+  wrapper: {
+    padding: 5,
+  },
 });
 
-export default function PlayersScreen({route}) {
+export default function PlayersScreen({route, navigation}) {
   const {team} = route.params;
   const {execute, pending, value, error} = useAsync(getPlayers(team), false);
 
@@ -23,7 +27,16 @@ export default function PlayersScreen({route}) {
 
   return (
     <View style={styles.container}>
-      <Text>{team}</Text>
+      <FlatList
+        contentContainerStyle={styles.wrapper}
+        numColumns={2}
+        horizontal={false}
+        data={value || []}
+        renderItem={({item, index}) => (
+          <PlayerCard navigation={navigation} index={index} data={item} />
+        )}
+        keyExtractor={(item) => item.code}
+      />
     </View>
   );
 }
---
+++ screens/Players/components/PlayerCard.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+    flex: 1,
+    maxWidth: '50%',
+  },
+  img: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 182 / 256,
+  },
+  content: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    left: 0,
+    bottom: 0,
+    // backgroundColor: 'red',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  face: {
+    width: 120,
+    height: 120,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  country: {
+    width: 35,
+    height: 35,
+  },
+  stat: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    width: '50%',
+  },
+  rowWrap: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  name: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 5,
+  },
+});
+
+const STATS_KEY = ['spd', 'sht', 'dri', 'str', 'pas', 'def'];
+
+export default function PlayerCard({data}) {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          style={styles.img}
+          source={require('../../../assets/playerBG.png')}
+        />
+        <View style={styles.content}>
+          <View style={styles.row}>
+            <View>
+              <Text style={styles.text}>{data.position}</Text>
+              <Text style={styles.text}>{data.stats && data.stats.ovr}</Text>
+              <Image style={styles.country} source={{uri: data.country}} />
+            </View>
+            <Image style={styles.face} source={{uri: data.logo}} />
+          </View>
+          <View style={styles.rowWrap}>
+            {STATS_KEY.map((key) => (
+              <Text key={key} style={styles.stat}>
+                {data.stats && data.stats[key]} {key.toUpperCase()}
+              </Text>
+            ))}
+          </View>
+          <Text style={styles.name}>
+            {data.name && data.name.toUpperCase()}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
